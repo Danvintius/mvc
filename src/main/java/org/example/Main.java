f28dc0db198cb976f7767c5d7c0a83af9c814914
Azerty <danila.94@mail.ru>
2024-04-29 23:24:19 +0300

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
 
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Main {
+    public static void main(String[] args) throws LifecycleException, IOException {
+        final Tomcat tomcat = new Tomcat();
+        final var baseDir = Files.createTempDirectory("tomcat");
+        baseDir.toFile().deleteOnExit();
+        tomcat.setBaseDir(baseDir.toAbsolutePath().toString());
+
+        final Connector connector = new Connector();
+        connector.setPort(9999);
+        tomcat.setConnector(connector);
+
+        tomcat.getHost().setAppBase(".");
+        tomcat.addWebapp("", ".");
+
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
